5442a89bc73e6ea9b03748bc820bfbb553b67c95
Pavel Glebik <g1ebikpasha@gmail.com>
2026-08-31 19:48:35 +0300

    Lesson 2 (security group/user_data basj script)
    Signed-off-by: Pavel Glebik <g1ebikpasha@gmail.com>

---
+++ Lesson_2/WebServer.tf
@@ -0,0 +1,50 @@
+provider "aws" {}
+
+resource "aws_instance" "my_web_server" {
+  ami           = "ami-0303e2e4a29f041a3"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_web_server_security_group.id]
+  user_data = <<EOF
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+EOF
+
+  tags = {
+    Name = "my_ubuntu_instance"
+    Project = "Terraform Lesson 2"
+  }
+}
+
+resource "aws_security_group" "my_web_server_security_group" {
+  name = "my_web_server_security_group"
+  description = "Security group for my web server"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_web_server_security_group"
+    Project = "Terraform Lesson 2"
+  }
+}
